52be30c02412aff449650562385cd7d5fff5224c
KD-happy <2507086134@qq.com>
2023-11-20 19:19:11 +0800

    删除不需要的

--- Video/偷拍/playlist/do.js
+++
@@ -1,4 +0,0 @@
-createMyElement(4);
-var title = '偷拍';
-document.title = title;
-document.querySelector('h1').innerText = title;
--- Video/咬人猫/playlist/do.js
+++
@@ -1,4 +0,0 @@
-createMyElement(22);
-var title = '咬人猫';
-document.title = title;
-document.querySelector('h1').innerText = title;
